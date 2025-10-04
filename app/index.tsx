6c1e8b1676293b43d0a5949b642029c0b93405e0
Jesus <psjesusangel@gmail.com>
2025-10-03 23:11:10 -0400

    feat: initialize expo-router structure

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View className="flex-1 bg-background items-center justify-center">
+      <Text className="text-white text-2xl">🥝 Kewit</Text>
+    </View>
+  );
+}
